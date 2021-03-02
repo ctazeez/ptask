979e2f87d42cc5ab3566b8fae4c50a322c6084fb
unknown <aazeez.ct@gmail.com>
2021-03-02 22:48:29 +0000

    updated

--- init_bash.sh
+++ init_bash.sh
@@ -6,5 +6,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE mydb;
     GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
     CREATE TABLE myTable(ZIP1 char(5),ZIP2 char(5)); 
-    COPY myTable FROM '/var/lib/myData/thezip.csv' WITH (FORMAT csv);
+    COPY myTable FROM '/tmp/google_activity_by_London_Borough.csv' WITH (FORMAT csv);
 EOSQL
