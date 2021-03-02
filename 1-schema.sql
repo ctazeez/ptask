35e8c542415592c9ec4330d93beb97db51eb4e61
unknown <aazeez.ct@gmail.com>
2021-03-02 23:19:26 +0000

    updated again

---
+++ 1-schema.sql
@@ -0,0 +1,15 @@
+CREATE USER myuser;
+    CREATE DATABASE mydb;
+    GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+
+    CREATE TABLE google_activity_by_london_borough (
+    date DATE,
+	area_name VARCHAR ( 50 ) ,
+	area_code VARCHAR ( 50 ) ,
+	retail_and_recreation_percent_change_from_baseline VARCHAR ( 255 ) ,
+    grocery_and_pharmacy_percent_change_from_baseline VARCHAR ( 255 ) ,
+    parks_percent_change_from_baseline VARCHAR ( 255 ) ,
+    transit_stations_percent_change_from_baseline VARCHAR ( 255 ) ,
+    workplaces_percent_change_from_baseline VARCHAR ( 255 ) ,
+    residential_percent_change_from_baseline VARCHAR ( 255 )
+    );
--- init_bash.sh
+++ init_bash.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER myuser;
-    CREATE DATABASE mydb;
-    GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+cd /tmp/ 
+wget -q https://data.london.gov.uk/download/google-mobility-by-borough/26d5821b-fcb6-4aae-af73-ee0596942d16/google_activity_by_London_Borough.csv"
 
-    CREATE TABLE google_activity_by_london_borough (
-    date DATE,
-	area_name VARCHAR ( 50 ) ,
-	area_code VARCHAR ( 50 ) ,
-	retail_and_recreation_percent_change_from_baseline VARCHAR ( 255 ) ,
-    grocery_and_pharmacy_percent_change_from_baseline VARCHAR ( 255 ) ,
-    parks_percent_change_from_baseline VARCHAR ( 255 ) ,
-    transit_stations_percent_change_from_baseline VARCHAR ( 255 ) ,
-    workplaces_percent_change_from_baseline VARCHAR ( 255 ) ,
-    residential_percent_change_from_baseline VARCHAR ( 255 )
-    );
-    COPY myTable FROM '/tmp/google_activity_by_London_Borough.csv' WITH (FORMAT csv);
-EOSQL
