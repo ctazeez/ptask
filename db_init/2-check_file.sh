878fd9dc659de19960df08671b446fc4c6944175
unknown <aazeez.ct@gmail.com>
2021-03-03 21:43:17 +0000

    changed

--- db_init/2-check_file.sh
+++ db_init/2-check_file.sh
@@ -7,7 +7,7 @@ cmd="$@"
 until [ -f /opt/filedata/google_activity_by_London_Borough.csv ] ; 
 do
   >&2 echo "File not downloaded yet - Please wait!"
-  sleep 1
+  sleep 5
 done
  
 >&2 echo "File downloaded!"
