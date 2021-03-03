0abac566837bdcd08e6b07a22ad09671d8173fb5
unknown <aazeez.ct@gmail.com>
2021-03-03 22:06:49 +0000

    changed

---
+++ db_init/2-data.sql
@@ -0,0 +1,18 @@
+
+CREATE USER myuser;
+CREATE DATABASE mydb;
+GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+
+CREATE TABLE google_activity_by_lb (
+date DATE,
+area_name VARCHAR ( 50 ) ,
+area_code VARCHAR ( 50 ) ,
+retail_and_recreation_percent_change_from_baseline VARCHAR ( 255 ) ,
+grocery_and_pharmacy_percent_change_from_baseline VARCHAR ( 255 ) ,
+parks_percent_change_from_baseline VARCHAR ( 255 ) ,
+transit_stations_percent_change_from_baseline VARCHAR ( 255 ) ,
+workplaces_percent_change_from_baseline VARCHAR ( 255 ) ,
+residential_percent_change_from_baseline VARCHAR ( 255 )
+);
+
+COPY google_activity_by_lb FROM '/opt/filedata/google_activity_by_London_Borough.csv' delimiter ',' csv header;
