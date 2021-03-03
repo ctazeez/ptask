a1e11edf3a4e20445c1e0ebf6e2de6654f6f2915
unknown <aazeez.ct@gmail.com>
2021-03-03 21:16:43 +0000

    changed

--- 2-check_file.sh
+++ 2-check_file.sh
@@ -6,9 +6,9 @@ cmd="$@"
  
 until [ -f /opt/filedata/google_activity_by_London_Borough.csv ] ; 
 do
-  >&2 echo "File not unavailable - sleeping"
+  >&2 echo "File not downloaded yet - Please wait!"
   sleep 1
 done
  
->&2 echo "File ready - executing command"
+>&2 echo "File downloaded!"
 exec $cmd
