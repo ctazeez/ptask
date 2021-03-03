421dd50c15d605824cd1a9acf2f980da158aee3f
unknown <aazeez.ct@gmail.com>
2021-03-03 12:57:15 +0000

    updated again

--- 2-init_bash.sh
+++ 2-init_bash.sh
@@ -6,7 +6,7 @@
 
 #apt-get install wget -y
 
-cd /tmp/ 
+#cd /tmp/ 
 
-wget -q https://data.london.gov.uk/download/google-mobility-by-borough/26d5821b-fcb6-4aae-af73-ee0596942d16/google_activity_by_London_Borough.csv"
+#wget -q https://data.london.gov.uk/download/google-mobility-by-borough/26d5821b-fcb6-4aae-af73-ee0596942d16/google_activity_by_London_Borough.csv"
 
