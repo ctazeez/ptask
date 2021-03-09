f70dde1906a0e135c61cf65a1e4109311b2fd2ba
unknown <aazeez.ct@gmail.com>
2021-03-09 15:37:13 +0000

    updated

--- make.sh
+++ make.sh
@@ -6,17 +6,17 @@ action=$1
 if [ "$action" == "up" ]
 then
     chmod -R +x db_init/
-    docker-compose up
+    #docker-compose up
 
 elif [ "$action" == "down" ]
 
-    docker-compose down
+    #docker-compose down
     docker volume rm  ptask_dbdata
     docker volume rm  ptask_filedata
 
 else
 then    
     echo "Please enter argument as up or down"
-    echo "up - To create the application."
-    echo "down - To destroy the containers and application."
+    echo "up - To create the application.
+          down - To destroy the containers and application."
 fi
