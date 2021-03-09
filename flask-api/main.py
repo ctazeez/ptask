21c9bff5b042abc77cf4cc23063990974fe2f2e7
unknown <aazeez.ct@gmail.com>
2021-03-09 10:07:07 +0000

    updated

--- flask-api/main.py
+++ flask-api/main.py
@@ -35,48 +35,102 @@ class google_activity(db.Model):
         self.workplaces_percent_change_from_baseline = workplaces_percent_change_from_baseline
         self.residential_percent_change_from_baseline = residential_percent_change_from_baseline
 
-@app.route('/healthcheck', methods=['GET'])
-def healthCheck():
-    return {
-        'Status':'Healthy'
-    }
 
-@app.route('/api/area', methods=['GET'])
-def getArea():
-    name = request.args.get('name')
-    # allarea = area.query.all()
-    #getarea = google_activity()
-    result=[]
-    allarea = google_activity.query.all()
-    #filtered = [a for a in allarea if a['area_name']==name]
-    for area in allarea:
+@app.route('/api/v1/healthcheck', methods=['GET'])
+def healthCheck():
+    try:
+        count = Activity.query.count()
+        return{
+            'Status':'Healthy'
+        }, 200
+    except exceptions.SQLAlchemyError:
+        print("Error while fetching data from PostgreSQL")
+        return{
+            'Status':'Not Healthy'
+        }, 404
+
+
+@app.route('/api/v1/activities', methods=['GET'])
+def activities():
+    output= []
+    
+    try:
+      
+        allactivity = Activity.query.all()
+
+        for acitivity in allactivity:
+            curActivity ={}
+            curActivity['Date'] = acitivity.date
+            curActivity['area_name'] = acitivity.area_name
+            curActivity['area_code'] = acitivity.area_code
+            curActivity['retail_and_recreation_percent_change_from_baseline'] = acitivity.retail_and_recreation_percent_change_from_baseline
+            curActivity['grocery_and_pharmacy_percent_change_from_baseline'] = acitivity.grocery_and_pharmacy_percent_change_from_baseline
+            curActivity['parks_percent_change_from_baseline'] = acitivity.parks_percent_change_from_baseline
+            curActivity['transit_stations_percent_change_from_baseline'] = acitivity.transit_stations_percent_change_from_baseline
+            curActivity['workplaces_percent_change_from_baseline'] = acitivity.workplaces_percent_change_from_baseline
+            curActivity['residential_percent_change_from_baseline'] = acitivity.residential_percent_change_from_baseline
+
+            output.append(curActivity)
+    except exceptions.SQLAlchemyError:
+        print("Error while fetching data from PostgreSQL", error)
+
+    return jsonify(output), 200
+
+@app.route('/api/v1/activities/area_by_code/<code>', methods=['GET'])
+def get_area_by_code(code):
+
+    try:
+        output = []
+        allactivity = Activity.query.filter_by(area_code=code)
+
+        for acitivity in allactivity:
+            curActivity ={}
+            curActivity['area_name'] = acitivity.area_name
+            curActivity['area_code'] = acitivity.area_code
+
+            output.append(curActivity)
+        
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    return jsonify(output), 200
+
+
+@app.route('/api/v1/activities/pointers',methods=['GET'])
+def getSearchData(): 
+    allarea = Activity.query.all()
+    keys = [k for k in allarea[0].__dict__]
+    del keys[0]
+    return jsonify(keys), 200
+
+
+@app.route('/api/v1/activities/pointers/<string:areacode>/<string:coloumn>/<string:pvalue>', methods=['GET'])
+def get_data_by_area_code_and_coloumn(areacode: str, coloumn: str, pvalue: str):
+    
+    result = []
+    
+    all_data = Activity.query.filter(getattr(Activity,coloumn).like("%" + pvalue + "%")).filter_by(area_code=areacode).all()
+
+    if len(all_data) <=0:
+        return{
+            "Message":"No result.. check the data passed."
+        }
+
+    for data in all_data:
         curActivity ={}
-        curActivity['area_name'] = area.area_name
-        curActivity['area_code'] = area.area_code
+        curActivity['area_name'] = data.area_name
+        curActivity['area_code'] = data.area_code
+        curActivity['date'] = data.date
+        curActivity['retail_and_recreation_percent_change_from_baseline'] = data.retail_and_recreation_percent_change_from_baseline
+        curActivity['grocery_and_pharmacy_percent_change_from_baseline'] = data.grocery_and_pharmacy_percent_change_from_baseline
+        curActivity['parks_percent_change_from_baseline'] = data.parks_percent_change_from_baseline
+        curActivity['transit_stations_percent_change_from_baseline'] = data.transit_stations_percent_change_from_baseline
+        curActivity['workplaces_percent_change_from_baseline'] = data.workplaces_percent_change_from_baseline
+        curActivity['residential_percent_change_from_baseline'] = data.residential_percent_change_from_baseline
         
         result.append(curActivity)
     return jsonify(result), 200
-    
-@app.route('/google_activity', methods=['GET'])
-def getGoogleActivity():
-    output=[]
-    allactivity = google_activity.query.all()
 
-    for acitivity in allactivity:
-        curActivity ={}
-        curActivity['Date'] = acitivity.date
-        curActivity['area_name'] = acitivity.area_name
-        curActivity['area_code'] = acitivity.area_code
-        curActivity['retail_and_recreation_percent_change_from_baseline'] = acitivity.retail_and_recreation_percent_change_from_baseline
-        curActivity['grocery_and_pharmacy_percent_change_from_baseline'] = acitivity.grocery_and_pharmacy_percent_change_from_baseline
-        curActivity['parks_percent_change_from_baseline'] = acitivity.parks_percent_change_from_baseline
-        curActivity['transit_stations_percent_change_from_baseline'] = acitivity.transit_stations_percent_change_from_baseline
-        curActivity['workplaces_percent_change_from_baseline'] = acitivity.workplaces_percent_change_from_baseline
-        curActivity['residential_percent_change_from_baseline'] = acitivity.residential_percent_change_from_baseline
-
-        output.append(curActivity)
-
-    return jsonify(output), 200
 
 if __name__ == "__main__":
     app.run('0.0.0.0',debug=True)
