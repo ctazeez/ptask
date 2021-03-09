e0fbc03df1f170bd7e2a681798897753c1d5c156
unknown <aazeez.ct@gmail.com>
2021-03-09 15:46:31 +0000

    updated

--- make.sh
+++ make.sh
@@ -6,14 +6,12 @@ action=$1
 if [ "$action" == "up" ]
 then
     chmod -R +x db_init/
-    #docker-compose up
-
+    docker-compose up
 elif [ "$action" == "down" ]
 then
-    #docker-compose down
+    docker-compose down
     docker volume rm  ptask_dbdata
     docker volume rm  ptask_filedata
-
 else    
     echo "Please enter argument as up or down"
     echo "
